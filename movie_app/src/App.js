61b4363de45491556f3adf844cb90f178fa59d44
sangwon0724 <leews0724@naver.com>
2021-06-04 17:07:10 +0900

    3.0 Class Components and State

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,56 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 import PropTypes  from "prop-types";
+import React from 'react';
 
-const foodILike = [
-  {
-    id: 1,
-    name: "Kimchi",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
-  },
-  {
-    id: 2,
-    name: "Samgyeopsal",
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
-  },
-  {
-    id: 3,
-    name: "Bibimbap",
-    image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
-  },
-  {
-    id: 4,
-    name: "Doncasu",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
-  },
-  {
-    id: 5,
-    name: "Kimbap",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
+
+
+class App extends React.Component {
+  state = {
+    count: 0
+  };
+  add = () => {
+    console.log("add");
+    this.state.count+=1;
+  };
+  minus = () => {
+    console.log("minus");
+    this.state.count-=1;
+  };
+  render() {
+    return (
+      <div>
+        <h1>The number is: <span id="num"></span></h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
   }
-];
-
-function Food({ name, picture }) {
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <img src={picture} alt={name} />
-    </div>
-  );
 }
 
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number
-};
-
-function App() {
+/*function App() {
   return (
     <div className="App">
       {foodILike.map(dish => (
@@ -58,10 +36,36 @@ function App() {
       ))}
     </div>
   );
-}
+}*/
+
+/*
+  컴포넌트 기본
+  function Food({ name, picture }) {
+    return (
+      <div>
+        <h2>I like {name}</h2>
+        <img src={picture} alt={name} />
+      </div>
+    );
+  }
+
+  Food.propTypes = {
+    name: PropTypes.string.isRequired,
+    picture: PropTypes.string.isRequired,
+    rating: PropTypes.number
+  };
+
+  <div className="App">
+      {foodILike.map(dish => (
+        <Food key={dish.id} name={dish.name} picture={dish.image} />
+      ))}
+    </div>
+*/
 
 
 /*
+  App return 기본
+
   <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -77,4 +81,5 @@ function App() {
         </a>
       </header>
 */
+
 export default App;
