82d143de44dd77e37e3833fe2643b2c3810af555
sangwon0724 <leews0724@naver.com>
2021-06-04 15:57:38 +0900

    start

---
+++ movie_app/src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      왔더
+    </div>
+  );
+}
+
+
+/*
+  <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+*/
+export default App;
