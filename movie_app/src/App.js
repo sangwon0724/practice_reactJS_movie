4834f2be241a891728437027de8bc5511d5b5e89
sangwon0724 <leews0724@naver.com>
2021-06-05 11:28:45 +0900

    6.1 Building the Router

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import {HashRouter, Route} from "react-router-dom";
+import Home from "./route/Home";
+import About from "./route/About";
 
 function App(){
-  return <span>Something</span>
+  return <HashRouter>
+    <Route path="/" component={Home} exact={true}/>
+    <Route path="/about" component={About} exact={true}/>
+  </HashRouter>
 }
 
 export default App;
--- movie_app/src/route/Home.js
+++ movie_app/src/route/Home.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from '../logo.svg';
 import './Home.css';
 //import PropTypes  from "prop-types";
 import React from 'react';
