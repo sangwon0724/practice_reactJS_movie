43191d56dbd5e6eee6ad30b921272f6434caa2b5
sangwon0724 <leews0724@naver.com>
2021-06-05 11:10:20 +0900

    6.0 Getting Ready for the Router

---
+++ movie_app/src/route/Home.js
@@ -0,0 +1,106 @@
+import logo from './logo.svg';
+import './Home.css';
+//import PropTypes  from "prop-types";
+import React from 'react';
+import axios from "axios";
+import Movie from "../components/Movie";
+
+
+
+class Home extends React.Component {
+  state = {
+    isLoading : true,
+    movies : []
+  };
+  getMovies = async () => {
+    const {data: {data: { movies }}}
+      = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    this.setState({ movies, isLoading: false });
+  };
+  componentDidMount() {
+    this.getMovies();
+  }
+  render() {
+    const { isLoading, movies } = this.state;
+    return(
+      <section className="container">
+        {isLoading
+          ? (
+            <div className="loader">
+              <span className="loader__text">Loading...</span>
+            </div>
+          )
+          : (
+            <div className="movies">
+              {movies.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              /> ))}
+              </div>
+            )}
+            </section>
+          )
+  }
+}
+
+/*function App() {
+  return (
+    <div className="App">
+      {foodILike.map(dish => (
+        <Food key={dish.id} name={dish.name} picture={dish.image} />
+      ))}
+    </div>
+  );
+}*/
+
+/*
+  컴포넌트 기본
+  function Food({ name, picture }) {
+    return (
+      <div>
+        <h2>I like {name}</h2>
+        <img src={picture} alt={name} />
+      </div>
+    );
+  }
+
+  Food.propTypes = {
+    name: PropTypes.string.isRequired,
+    picture: PropTypes.string.isRequired,
+    rating: PropTypes.number
+  };
+
+  <div className="App">
+      {foodILike.map(dish => (
+        <Food key={dish.id} name={dish.name} picture={dish.image} />
+      ))}
+    </div>
+*/
+
+
+/*
+  App return 기본
+
+  <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+*/
+
+export default Home;
